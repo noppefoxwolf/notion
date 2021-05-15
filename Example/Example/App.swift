b90657c73a432f3f3a3191eed6311d2935859861
noppe <noppelabs@gmail.com>
2021-05-15 23:06:40 +0900

    revoke token

--- Example/Example/App.swift
+++ Example/Example/App.swift
@@ -14,7 +14,7 @@ struct App: SwiftUI.App {
         WindowGroup {
             RootView(viewModel: .init()).environment(\.notion, {
                 let session = Session.shared
-                session.setAuthorization(token: "secret_LDF1jpWlRgbQv4gQEmH2bH6YhrlE7al84E0R4xkBDtr")
+                session.setAuthorization(token: "")
                 return session
             }())
         }
